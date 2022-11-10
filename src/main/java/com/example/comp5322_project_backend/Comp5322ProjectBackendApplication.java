e0057119d42c4ab21828c3810ad2245645aa7e1b
thomaskwok01 <thomaskwokwaitak@gmail.com>
2022-11-10 10:24:03 +0800

    Init commit

---
+++ src/main/java/com/example/comp5322_project_backend/Comp5322ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.comp5322_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp5322ProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp5322ProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comp5322_project_backend/Comp5322ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comp5322_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp5322ProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
